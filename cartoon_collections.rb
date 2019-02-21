4edebea53b6b6101351a6ec669030005fa1dccae
Mickae Truman <mtruman@nyit.edu>
2019-02-21 17:48:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,25 +12,11 @@ def summon_captain_planet(planeteer_calls)
 end
 
 
-#def long_planeteer_calls(array)
-  #if long_planeteer_calls.length > 4
-  #  puts true 
-  #else
-  #  puts false
-#end
-#end
-
-short_words = ["puff", "go", "two"]
-assorted_words = ["two", "go", "industrious", "bop"]
-if short_words.length > 4
-  puts true
-else 
-  puts false
-  if assorted_words.length > 4
-    puts true
-  else 
-    puts false
+def long_planeteer_calls(array)
+  array.any? {|array|array.length > 4}
 end
+
+
 
 def find_the_cheese(cheese)
  cheese_types = ["cheddar","gouda","cambert"]
